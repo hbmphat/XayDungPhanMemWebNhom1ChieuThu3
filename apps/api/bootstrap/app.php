4d696156366e60ac0e57d673ac681d75de9b72a5
Huynh Trung Hieu <kjhieu14@gmail.com>
2026-04-02 10:16:13 +0700

    fix: install sanctum and enable cors middleware

--- apps/api/bootstrap/app.php
+++ apps/api/bootstrap/app.php
@@ -27,8 +27,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
-
-        $middleware->statefulApi();
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
