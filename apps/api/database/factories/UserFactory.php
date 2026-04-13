2ad763abd467aaf44b9e56b8e22062f77a368a9f
Huynh Trung Hieu <kjhieu14@gmail.com>
2026-04-14 00:51:32 +0700

    chore: update user factory

--- apps/api/database/factories/UserFactory.php
+++ apps/api/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -23,18 +24,20 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = $this->faker ?? \Faker\Factory::create();
+
         return [
             'id' => Str::uuid(),
-            'user_name' => $this->faker->unique()->userName(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'phone' => '0'.$this->faker->numberBetween(320000000, 999999999),
-            'first_name' => $this->faker->firstName(),
-            'last_name' => $this->faker->lastName(),
-            'address' => $this->faker->address(),
-            'date_of_birth' => $this->faker->dateTimeBetween('-40 years', '-18 years')->format('Y-m-d'),
-            'password' => 'password',
-            'role' => $this->faker->randomElement(['inventory_manager', 'moderator', 'customer', 'collaborator']),
-            'status' => $this->faker->randomElement(['active', 'inactive', 'blocked', 'pending', 'pending_kyc', 'suspended', 'banned']),
+            'user_name' => $faker->unique()->userName(),
+            'email' => $faker->unique()->safeEmail(),
+            'phone' => '0'.$faker->numberBetween(320000000, 999999999),
+            'first_name' => $faker->firstName(),
+            'last_name' => $faker->lastName(),
+            'address' => $faker->address(),
+            'date_of_birth' => $faker->dateTimeBetween('-40 years', '-18 years')->format('Y-m-d'),
+            'password' => static::$password ??= Hash::make('password'),
+            'role' => $faker->randomElement(['inventory_manager', 'moderator', 'customer', 'collaborator']),
+            'status' => $faker->randomElement(['active', 'inactive', 'blocked', 'pending', 'pending_kyc', 'suspended', 'banned']),
             'remember_token' => Str::random(10),
         ];
     }
